d4e43f24f7b32ba4fb8a6a78df913e541d8325b5
Miikka-Alatalo <alatalo.miikka@gmail.com>
2018-05-23 17:24:59 +0300

    renaming

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-readme
+P5.js doodles
+
+Inspiration / reference / stuff from CodeTrain https://www.youtube.com/channel/UCvjgXvBlbQiydffZU7m1_aw
--- src/sketches/HelloWorld.js
+++ src/sketches/HelloWorld.js
@@ -1,10 +1,10 @@
-export default function sketch(p) {
-  p.setup = () => {
-    p.createCanvas(200, 200);
-    p.background(0)
+export default function sketch(_) {
+  _.setup = () => {
+    _.createCanvas(200, 200);
+    _.background(0)
   };
 
-  p.draw = () => {
-    p.rect(p.width/2-10, p.height/2-10, 20, 20)
+  _.draw = () => {
+    _.rect(_.width/2-10, _.height/2-10, 20, 20)
   };
 };
