2f3b16aca5f12d0a734f3b903e1012c4b95169cb
Miikka-Alatalo <alatalo.miikka@gmail.com>
2018-05-25 15:04:22 +0300

    working snake

--- src/sketches/Snake.js
+++ src/sketches/Snake.js
@@ -1,40 +1,147 @@
 export default function sketch(_) {
 
   _.setup = () => {
-    _.createCanvas(800, 800);
-    _.snek = new Snake(_);
-    _.frameRate(5);
+    _.createCanvas(600, 600);
+    _.apple = new Apple(_);
+    _.snake = new Snake(_);
+    _.frameRate(4);
   };
 
   _.draw = () => {
-    _.background(60)
-    _.snek.move();
-    _.snek.draw();
+    _.appleHitReg();
+    _.background(60);
+    _.apple.draw();
+    _.snake.draw();
+    _.snake.move();
+    _.snakeHitReg();
   };
 
-  _.keyPressed=()=>{
-    if(_.keyCode === _.UP_ARROW){
-      console.log("up");
-    } else {
-      console.log("nope");
+  _.appleHitReg = () => {
+    if (_.snake.x === _.apple.x && _.snake.y === _.apple.y) {
+      _.snake.snakeLength++;
+
+      while (true) {
+        let pos = {
+          x: Math.floor(Math.random() * 30) * 20,
+          y: Math.floor(Math.random() * 30) * 20
+        };
+        if (_.isFreeSpot(pos)) {
+          _.apple.newPosition(pos);
+          break;
+        }
+      }
+
     }
   }
+
+  _.snakeHitReg = () => {
+    if(_.dieded()){
+      console.log("dieded");
+      _.noLoop();
+    }
+  }
+
+  _.isFreeSpot = (pos) => {
+    if (_.snake.x === pos.x && _.snake.y === pos.y) {
+      return false;
+    }
+    return _.snake.snakeBody.filter(b => { return b.x === pos.x && b.y === pos.y}).length === 0;
+  }
+
+  _.dieded = () => {
+    for(let i =2;i<_.snake.snakeBody.length;i++){
+      if(_.snake.snakeBody[i].x===_.snake.x && _.snake.snakeBody[i].y ===_.snake.y){
+        return true;
+      }
+    }
+    return false;
+  }
+
 
 };
 
 function Snake(_) {
-  this.x = 0;
-  this.y = 0;
-  this.xs = 10;
+  this.snakeSize = 20;
+  this.snakeLength = 3;
+  this.snakeBody = [{x:80,y:100},{x:60,y:100},{x:40,y:100}];
+  this.x = 100;
+  this.y = 100;
+  this.xs = 20;
   this.ys = 0;
+  this.lastKey = _.RIGHT_ARROW;
+  this.hasMoved = true;
+
+  _.keyPressed = () => {
+    if (this.hasMoved) {
+      if (_.keyCode === _.UP_ARROW && this.lastKey !== _.DOWN_ARROW) {
+        this.lastKey = _.UP_ARROW;
+        this.resetSpeed();
+        this.ys = -20;
+      } else if (_.keyCode === _.DOWN_ARROW && this.lastKey !== _.UP_ARROW) {
+        this.lastKey = _.DOWN_ARROW;
+        this.resetSpeed();
+        this.ys = +20;
+      } else if (_.keyCode === _.RIGHT_ARROW && this.lastKey !== _.LEFT_ARROW) {
+        this.lastKey = _.RIGHT_ARROW;
+        this.resetSpeed();
+        this.xs = +20;
+      } else if (_.keyCode === _.LEFT_ARROW && this.lastKey !== _.RIGHT_ARROW) {
+        this.lastKey = _.LEFT_ARROW;
+        this.resetSpeed();
+        this.xs = -20;
+      }
+    }
+  }
+
+  this.resetSpeed = () => {
+    this.xs = 0;
+    this.ys = 0;
+    this.hasMoved = false;
+  }
 
   this.move = () => {
+    this.hasMoved = true;
+    this.snakeBody.unshift({
+      x: this.x,
+      y: this.y
+    });
+    if (this.snakeBody.length > this.snakeLength) {
+      this.snakeBody.pop();
+    }
     this.x += this.xs;
     this.y += this.ys;
+    if (this.x > _.width - this.snakeSize) {
+      this.x = 0;
+    } else if (this.x < 0) {
+      this.x = _.width - this.snakeSize
+    } else if (this.y > _.height - this.snakeSize) {
+      this.y = 0;
+    } else if (this.y < 0) {
+      this.y = _.height - this.snakeSize;
+    }
   }
 
   this.draw = () => {
+    _.fill(200);
+    this.snakeBody.forEach(b => _.rect(b.x, b.y, this.snakeSize, this.snakeSize));
     _.fill(255);
-    _.rect(this.x, this.y, 10, 10);
+    _.rect(this.x, this.y, this.snakeSize, this.snakeSize);
+  }
+}
+
+function Apple(_) {
+  this.appleSize = 16;
+  this.x = Math.floor(Math.random() * 30) * 20;
+  this.y = Math.floor(Math.random() * 30) * 20;
+
+  this.newPosition = (pos) => {
+    this.x = pos.x;
+    this.y = pos.y;
+  }
+
+  this.draw = () => {
+    _.fill(0, 255, 0);
+    // (snakeSize - appleSize) / 2 = 2
+    _.rect(this.x + 2, this.y + 2, this.appleSize, this.appleSize);
   }
 }
