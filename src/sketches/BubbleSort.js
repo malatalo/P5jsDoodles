72702c7b670fd2d6c6f1a1effe33ea45851ff0e7
Miikka Alatalo <alatalo.miikka@gmail.com>
2018-10-05 13:07:03 +0300

    add Bubblesort

---
+++ src/sketches/BubbleSort.js
@@ -0,0 +1,11 @@
+export default function sketch(_) {
+
+  _.setup = () => {
+    _.size(600, 600);
+  };
+
+  _.draw = () => {
+    _.background(0);
+  };
+
+};
--- src/sketches/index.js
+++ src/sketches/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "Bubble Sort": require("./BubbleSort").default,
   "Langton's Ant": require('./Langtonsant').default,
   "Snake": require('./Snake').default,
   "Hello World": require('./HelloWorld').default,
